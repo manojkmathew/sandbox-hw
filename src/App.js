ebc1684c14a16e0f8cf60a373fa125caea647891
manojkmathew <86129379+manojkmathew@users.noreply.github.com>
2021-07-16 08:14:34 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello from MM2</h1>
+      <a href="https://zephora-react.herokuapp.com/">Zephora</a>
     </div>
   );
 }
